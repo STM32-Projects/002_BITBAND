a21b983a72244e970ef1c8bcda5468da1198b2c8
Supreeth <supreethanil128@gmail.com>
2020-09-12 13:12:53 +0530

    Add mapping formula for bit-band in the comments

--- inc/stm32f446xx_bitband.h
+++ inc/stm32f446xx_bitband.h
@@ -12,6 +12,39 @@
   * This file contains macros for using bitband feature for stm32f446re
   * Nucleo Boards.
   *
+  * The following contents are copied from STM32F446xx Reference Manual.
+  *
+  * A mapping formula shows how to reference each word in the alias region
+  * to a corresponding bit in the bit-band region. The mapping formula is:
+  *
+  * bit_word_addr = bit_band_base + (byte_offset x 32) + (bit_number × 4)
+  *
+  * where:
+  *
+  * --> bit_word_addr is the address of the word in the alias memory region
+  *     that maps to the targeted bit.
+  *
+  * --> bit_band_base is the starting address of the alias region.
+  *
+  * --> byte_offset is the number of the byte in the bit-band region that
+  *     contains the targeted bit.
+  *
+  * --> bit_number is the bit position (0-7) of the targeted bit.
+  *
+  * Example
+  * --------
+  *
+  * --> The following example shows how to map bit 2 of the byte located at SRAM address
+  *     0x20000300 to the alias region:
+  *
+  * 0x22006008 = 0x22000000 + (0x300*32) + (2*4)
+  *
+  * Writing to address 0x22006008 has the same effect as a read-modify-write operation
+  * on bit 2 of the byte at SRAM address 0x20000300.
+  *
+  * Reading address 0x22006008 returns the value (0x01 or 0x00) of bit 2 of the byte
+  * at SRAM address 0x20000300 (0x01: bit set; 0x00: bit reset).
+  *
   * Runs on stm32f446re
   *
   ******************************************************************************
